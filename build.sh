846d1bf714377e1d0a829c39d91fbc67e51e87b3
chenk <chenk@apical.com.cn>
2024-11-29 21:00:36 +0800

    修改 build.sh 编译脚本
    Change-Id: Ie2d78fa72593847a114de8259f09841eb9d8aa95

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 set -e
 
-gcc -Wall utils.c ffosm.c main.c -I$PWD/ffhttpd/include -L$PWD/ffhttpd/lib -lffhttpd -lsqlite3 -lpthread -o ffosm
+gcc -Wall -g -Os utils.c ffosm.c main.c -I$PWD/ffhttpd/include -L$PWD/ffhttpd/lib -lffhttpd -lsqlite3 -lpthread -o ffosm
+strip ffosm
